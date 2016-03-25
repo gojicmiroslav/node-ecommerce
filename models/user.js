9501a58d59e2c6046c7ccf9cdb6f8866256cf9e4
gojicmiroslav <miroslavy2k@gmail.com>
2016-03-25 01:41:43 +0100

    Create UserSchema  - install Mongoose and Bcrypt-nodejs  - connect to MongoLab

---
+++ models/user.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+/* The user schema attributes */
+var UserSchema = new Schema({
+	email: { type.String, unique: true, lowercase: true },
+	password: String,
+	profile: {
+		name: { type: String, default: '' },
+		picture: { type: String, default: '' }
+	},
+	address: String,
+	history: [{
+		date: Date,
+		paid: { type: Number, default: 0 }
+	}]
+});
+
+/* Hash the password before we even save it to the database */
+// Schema#pre(method, callback)
+// Defines a pre hook for the document
+UserSchema.pre('save', function(next){
+	var user = this; // this -> reffers to UserSchema
+
+	// Schema#isModified() - Returns true if this document was modified, else false.
+	if(!user.isModified('password')){
+		return next();
+	}
+
+	bcrypt.genSalt(10, function(err, salt){
+		if(err) return next(err);
+
+		// hash(data, salt, progress, cb)
+		bcrypt.hash(user.password, salt, null, function(err, hash){
+			if(err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+/* compare password in the database and the one the the user type in. */
+// We create custom method - add keyword methods to Schema name
+UserSchema.methods.comparePassword = function(password){
+	// compareSync(data, encrypted) - data(data to compare), encrypted(data to be compare to)
+	return bcrypt.compareSync(password, this.password);
+};
+
+// To use schema definition, we need to convert UserSchema into a Model we can work with. 
+// To do so, we pass it into mongoose.model(modelName, schema):
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://root:deronje@ds019628.mlab.com:19628/ecommerce_misa', function(err){
+	if(err) {
+		console.log(err);
+	} else {
+		console.log('Connected to the database');
+	}	
+
+
+});
 
 //add morgan middleware
 app.use(morgan('dev'));
