0572269e7ffc84d5562ae1d1ece714d9568b392f
gojicmiroslav <miroslavy2k@gmail.com>
2016-04-14 23:45:23 +0200

    Add mongoosastic library and indexed Product documents to Elasticsearch replica

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,34 @@
 var router = require('express').Router();
 var Product = require('../models/product');
 
+// creating bridge between DB and Elasticsearch replica set
+Product.createMapping(function(err, mapping){
+	if(err) {
+		console.log('Error creating mapping');
+		console.log(err);
+	} else {
+		console.log("Mapping created");
+		console.log(mapping);
+	}
+});
+
+var stream = Product.synchronize();
+var count = 0;
+
+// count documents
+stream.on('data', function(){
+	count++;
+});
+
+stream.on('close', function(){
+	console.log("Indexed " + count + " documents");
+});
+
+stream.on('error', function(err){
+	console.log(err);
+});
+
+
 router.get('/', function(req, res){
 	res.render('./main/home');
 });
