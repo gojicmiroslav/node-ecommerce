82d57c98ac79c253d2020fb8c614b721005b6ba1
gojicmiroslav <miroslavy2k@gmail.com>
2016-05-25 22:56:02 +0200

    Add a spinner to payment dialog

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,30 @@
 $(function(){
 	Stripe.setPublishableKey('pk_test_tljppQ1Bsb0MN9Vm2awiPEyq');
 
+	// spinner options
+	var opts = {
+		  lines: 13 // The number of lines to draw
+		, length: 56 // The length of each line
+		, width: 16 // The line thickness
+		, radius: 51 // The radius of the inner circle
+		, scale: 1 // Scales overall size of the spinner
+		, corners: 1 // Corner roundness (0..1)
+		, color: '#000' // #rgb or #rrggbb or array of colors
+		, opacity: 0.1 // Opacity of the lines
+		, rotate: 0 // The rotation offset
+		, direction: 1 // 1: clockwise, -1: counterclockwise
+		, speed: 0.7 // Rounds per second
+		, trail: 60 // Afterglow percentage
+		, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+		, zIndex: 2e9 // The z-index (defaults to 2000000000)
+		, className: 'spinner' // The CSS class to assign to the spinner
+		, top: '50%' // Top position relative to parent
+		, left: '50%' // Left position relative to parent
+		, shadow: false // Whether to render a shadow
+		, hwaccel: false // Whether to use hardware acceleration
+		, position: 'absolute' // Element positioning
+	}
+
 	$('#search').on('keyup', function(){
 		var search_term = $(this).val();
 
@@ -106,8 +130,8 @@ $(function(){
 	    // Insert the token into the form so it gets submitted to the server
 	    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
 
-	    //var spinner = new Spinner(opts).spin();
-	    //$('#loading').append(spinner.el);
+	    var spinner = new Spinner(opts).spin();
+	    $('#loading').append(spinner.el);
 	    // and submit(re-submit the form to stripe)
 	    $form.get(0).submit();
 	  }
